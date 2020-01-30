9879c850fa41a84dfb2facd53470199994d4da52
Don Cote <mistercote@gmail.com>
2020-01-29 20:46:43 -0500

    default segue to empty string

--- app/serializers/track_serializer.rb
+++ app/serializers/track_serializer.rb
@@ -2,7 +2,8 @@ class TrackSerializer < Blueprinter::Base
   #identifier :id
 
   view :setlist do
-    fields :set, :position, :segue, :song_id, :song_title, :song_slug
+    fields :set, :position, :song_id, :song_title, :song_slug
+    field :segue, default: ""
     field :annotations do |track, options|
       track.annotations.map(&:desc)
     end
