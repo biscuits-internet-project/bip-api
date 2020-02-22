cc079c095736b5be42f0d1f53f4af56e29d91d2e
Don Cote <mistercote@gmail.com>
2020-02-22 11:09:21 -0500

    fix venue update times played

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -10,7 +10,7 @@ class Venue < ApplicationRecord
   scope :city, -> (city, state) { where(city: city, state: state) }
   scope :state, -> (state) { where(state: state) }
 
-  after_save :update_column, :expire_venue_all_cache
+  after_save :update_times_played, :expire_venue_all_cache
   after_destroy :expire_venue_all_cache
 
   def expire_venue_all_cache
