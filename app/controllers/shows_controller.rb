61724f09579e2fb249310f9277d47edec1580d07
Don Cote <mistercote@gmail.com>
2020-02-22 13:14:29 -0500

    fix latest shows track order

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -7,7 +7,7 @@ class ShowsController < ApplicationController
   def index
     if params[:last].present?
       ids = Show.order("date desc").limit(params[:last].to_i)
-      shows = Show.where(id: ids).to_a
+      shows = Show.includes(:venue, tracks: [:annotations, :song]).merge(Track.setlist).where(id: ids).to_a
       shows = shows.sort {|a,b| b.date <=> a.date }
     else
       shows = Show.includes(:venue, tracks: [:annotations, :song]).merge(Track.setlist)
