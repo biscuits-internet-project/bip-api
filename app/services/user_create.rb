150eec892980fbab7ca5332002fda1d4f0eb0785
Don Cote <mistercote@gmail.com>
2020-03-04 23:26:20 -0500

    enable avatar save

--- app/services/user_create.rb
+++ app/services/user_create.rb
@@ -8,15 +8,11 @@ class UserCreate
   end
 
   def call
-    avatar = params[:avatar]
-    user = User.new(params.except(:avatar))
+    user = User.new(params)
     # auto-confirm since this is admin only
     user.confirmed_at = DateTime.now
-  
+
     if user.save
-      if avatar.present?
-        user.avatar.attach(avatar)
-      end
       return user
     else
       errors.merge!(user.errors)
