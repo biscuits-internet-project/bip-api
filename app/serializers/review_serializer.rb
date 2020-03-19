6359253acf307276f8b1ca9da391809cf9779811
Don Cote <mistercote@gmail.com>
2020-03-19 13:56:49 -0400

    update gems, modify review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,10 @@
 class ReviewSerializer < Blueprinter::Base
   identifier :id
 
-  fields :id, :content, :reviewable_type, :reviewable_id
+  fields :id, :content, :created_at, :updated_at
   association :user, blueprint: UserSerializer, view: :public
+
+  field :show_id do |review, options|
+    review.reviewable_id
+  end
 end
