988003cb9076f38a192a691025999c9fd3cfd025
Don Cote <mistercote@gmail.com>
2020-05-09 15:34:38 -0400

    add timestamps to blog post serializer

--- app/serializers/blog_post_serializer.rb
+++ app/serializers/blog_post_serializer.rb
@@ -2,7 +2,7 @@ class BlogPostSerializer < Blueprinter::Base
   identifier :id
 
   fields :id, :title, :slug, :blurb, :content, :state, :published_at,
-   :primary_image_url, :secondary_image_url
+   :primary_image_url, :secondary_image_url, :created_at, :updated_at
   association :user, blueprint: UserSerializer, view: :public
   field :tag_list, name: :tags do |track, options|
     track.tags.map(&:name)
